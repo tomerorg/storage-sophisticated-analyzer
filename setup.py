de38af5208f891aeecd0d5607e99c5ed52ed34e9
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-16 13:55:40 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="storage-sophisticated-analyzer",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="Experimental storage project using SQLAlchemy and Django and Pandas in Python",
+    keywords="storage-sophisticated-analyzer, python",
+    url="",
+)
